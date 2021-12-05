901be6dcd282ae2bc8fe566799dc40cdf5ca08d6
modopo <modopo@gmail.com>
2021-12-04 16:47:37 -0800

    Completed day 1

--- Day_1/advent_1.js
+++ Day_1/advent_1.js
@@ -6,13 +6,20 @@ function loadIntoArray(fileName) {
     return data;
 }
 
+function threeWindowSum(array) {
+    let result = [];
+
+    for (let index = 0; index < array.length - 2; index++) {
+        result.push(Number(array[index]) + Number(array[index + 1]) + Number(array[index + 2]));
+    }
+
+    return result;
+}
+
 function countIncrease(data) {
     let increase = 0;
 
-
     data.reduce((prev, current) => {
-        console.log(prev);
-        console.log(current);
         if (Number(prev) < Number(current)) {
             increase++;
         }
@@ -23,4 +30,4 @@ function countIncrease(data) {
     return increase;
 }
 
-console.log(countIncrease(loadIntoArray('day1_input.txt')));
+console.log(countIncrease(threeWindowSum(loadIntoArray('day1_input.txt'))));

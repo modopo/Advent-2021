6afdff50d67a89304018949b73da97b29c1db596
modopo <modopo@gmail.com>
2021-12-12 22:15:06 -0800

    Created board

--- Day_5/overlap.js
+++ Day_5/overlap.js
@@ -17,6 +17,53 @@ function parseCoordinate(input) {
 
 }
 
-let raw = loadInput('test_input.txt');
+function determineBoard(parsed) {
+    let number = [];
 
-console.log(parseCoordinate(raw));
+    parsed.forEach(entry => {
+        entry.forEach(coord => {
+            coord.split(',').forEach(num => {
+                number.push(num);
+            });
+        })
+    });
+
+    let x = 0;
+    let y = 0;
+
+    for (let index = 0; index < number.length; index++) {
+        if (index % 2 === 0) {
+            if (number[index] > x) {
+                x = number[index];
+            }
+        } else if (index % 2 !== 0) {
+            if (number[index] > y) {
+                y = number[index];
+            }
+        }
+    };
+
+    return [Number(x), Number(y)];
+
+}
+
+function makeBoard(size) {
+    let board = [];
+
+    for (let index = 0; index < 3; index++) {
+        let inner = []
+        for (let index2 = 0; index2 < 3; index++) {
+            inner.push(0);
+        }
+        board.push(inner);
+    }
+
+    return board;
+}
+
+let raw = loadInput('test_input.txt');
+let parsed = parseCoordinate(raw);
+let boardSize = determineBoard(parsed);
+
+console.log(determineBoard(parsed));
+console.log(makeBoard([3, 3]));

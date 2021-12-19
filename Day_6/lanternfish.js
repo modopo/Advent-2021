61fea34e8cb97a83ebdf347935cacdfecae11d09
modopo <modopo@gmail.com>
2021-12-19 14:36:13 -0800

    Completed day 6

--- Day_6/lanternfish.js
+++ Day_6/lanternfish.js
@@ -1,4 +1,6 @@
+const { count } = require('console');
 const fs = require('fs');
+const { totalmem } = require('os');
 
 function intake(filename) {
     let result = fs.readFileSync(filename).toString().split(',').map(number => {
@@ -32,10 +34,26 @@ function grow(arr, days) {
         result = temp;
     }
 
-    return result.length;
+    return result;
+}
+
+function largeCount(arr, days) {
+    let counts = Array(9).fill(0);
+
+    arr.forEach(number => {
+        counts[number] += 1;
+    })
+
+    for (let i = 0; i < days; i++) {
+        const newCount = counts.shift();
+        counts[6] += newCount;
+        counts.push(newCount);
+    }
+
+    return counts.reduce((count, total) => count + total);
 }
 
 let raw = intake('day6_input.txt');
-let result = grow(raw, 256);
+let result = largeCount(raw, 256);
 
 console.log(result);

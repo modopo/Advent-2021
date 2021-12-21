c664682de9de945f7c4bd17ef70f1374afdf799a
modopo <modopo@gmail.com>
2021-12-21 01:20:24 -0800

    Determine upper, lower and middle

--- Day_7/crab.js
+++ Day_7/crab.js
@@ -6,4 +6,24 @@ function intake(filename) {
     return result;
 }
 
-console.log(intake('test.txt'));
+function middle(arr) {
+    let sorted = arr.sort((a, b) => a - b);
+
+    return sorted[(Math.floor(sorted.length / 2))];
+}
+
+function calculateFuel(arr, point) {
+    let fuel = 0;
+
+    arr.forEach(position => {
+        fuel += Math.abs(position - point);
+    })
+
+    return fuel;
+}
+
+
+
+let raw = intake('test.txt');
+
+console.log(calculateFuel(raw, middle(raw)));
